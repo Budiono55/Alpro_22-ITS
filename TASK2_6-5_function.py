695fa746e331aace459a466e39a9ac4fe5ff95e2
Budiono55 <zakizaidan304@ymail.com>
2023-04-13 11:45:50 +0700

    sorted function blom selese

---
+++ TASK2_6-5_function.py
@@ -0,0 +1,10 @@
+#6.5 
+
+num1, num2, num3 = map(eval, (input("Enter three numbers:").split()))
+
+def sorted(num1, num2, num3):
+    print("handsome")
+
+print(sorted(num1, num2, num3))
+print(num1, num2, num3)
+
--- spring_series.py
+++ spring_series.py
@@ -2,7 +2,7 @@ K1 = eval(input("K1 constant "))
 K2 = eval(input("K2 constant "))
 K3 = eval(input("K3 constant "))
 
-F = eval(input("Force desired: "))
+F = float(input("Force desired: "))
 
 spring_series= 1/((1/K1)+(1/K2)+(1/K3))
 
