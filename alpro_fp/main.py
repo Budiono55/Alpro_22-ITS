f6bc6b8ab720e7bfe01ee7e386374011e0cd250e
Budiono55 <zakizaidan304@ymail.com>
2023-06-21 19:33:22 +0700

    pass gen

--- alpro_fp/main.py
+++ alpro_fp/main.py
@@ -1,27 +1,72 @@
 import random
+from tkinter import *
 
+length = 0
+p_short = 8
+p_medium = 12 #med kapital angka
+p_hard = 14
+p_num = 4
+
+root = Tk()
+root.geometry("175x175")
+
+#Everyday iam Shuffleeeeeing
 def shuffle(string):
     tempList = list(string)
     random.shuffle(tempList)
     return ''.join(tempList)
 
-length = 0
-p_short = 8
-p_medium = 10
-p_hard = 12
+#easy version
+def passhort():
+    user_p = ''
+    if len(user_p) <= p_short:
+        while len(user_p)<= p_short-p_num:
+            user_p += chr(random.randint(ord('a'), ord('z')))
+        while len(user_p)<= p_short:
+            user_p += str(random.randint(0,9))
+    user_p2 = shuffle(user_p)
+    return user_p2
 
-while  length <= p_short:
-     print(12)
-     length+=1
+#medium version
+def pasmedium():
+    user_p = ''
+    if len(user_p) <= p_medium:
+        while len(user_p)<= p_medium-p_num:
+            user_p += chr(random.randint(65,90))
+        while len(user_p)<= p_medium:
+            user_p += str(random.randint(0,9))
+    user_p2 = shuffle(user_p)
+    return user_p2
 
-Letter1 = chr(random.randint(65,90))
-Letter2 = chr(random.randint(65,90))
-Letter3 = chr(random.randint(65,90))
-Letter4 = chr(random.randint(65,90))
-Letter5 = chr(random.randint(65,90))
-Letter6 = chr(random.randint(65,90))
+#hard version
+def pashard():
+    user_p = ''
+    if len(user_p) <= p_hard:
+        while len(user_p)<= p_hard-p_num:
+            user_p += chr(random.randint(65,90))
+        while len(user_p)<= p_hard:
+            user_p += str(random.randint(0,9))
+    user_p2 = shuffle(user_p)
+    return user_p2
 
-password = Letter1 + Letter2
-#password = shuffle(password)
+v = StringVar(root, "1")
 
-print(password)
+value = {
+    "Easy":passhort(),
+    "Medium":pasmedium(),
+    "Hard":pashard()
+}
+
+# for(text, value) in value.items():
+#     Radiobutton(root, text = text, variable = v,
+#                 value = value, indicator = 0,
+#                 background="white").pack(fill = X, ipady = 5)
+
+#print(len(user_p))
+# password= shuffle(pasmedium())
+# print(pasmedium())
+# print(password)
+
+print(value["Easy"])
+
+#mainloop()
