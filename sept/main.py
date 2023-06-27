2dc881e0e4f62e5b84f234a67921048da1c62143
Budiono55 <zakizaidan304@ymail.com>
2023-06-28 00:07:37 +0700

    add init

---
+++ sept/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import matplotlib
+import math
+
+root = Tk()
+root.title('Parabolic Function')
+root.resizable(False, False)
+root.geometry("500x500")
+
+root.mainloop()
